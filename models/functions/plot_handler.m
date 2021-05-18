8fc3da986bbb185a453cdb2082e91d2e71675c34
xvoron <artem_voronin95@mail.ru>
2021-05-19 01:14:08 +0200

    Latex Update: Models, SB

--- models/functions/plot_handler.m
+++ models/functions/plot_handler.m
@@ -1,20 +1,21 @@
 function [] = plot_handler(health, fault, title_string)
-    figure
+    f = figure;
+    f.Position(3:4) = [1000 500];
     hold on
     plot(health(1).data_measured{1,1}.Time, ...
-        health(1).data_measured{1,1}.Data, 'k', 'DisplayName', 'reference')
+        health(1).data_measured{1,1}.Data, 'k', 'LineWidth', 2, 'DisplayName', 'reference')
     plot(health(1).data_simulated{1,1}.Time, ...
-        health(1).data_simulated{1,1}.Data, 'k--', 'DisplayName', 'reference sim')
+        health(1).data_simulated{1,1}.Data, 'k--', 'LineWidth', 2, 'DisplayName', 'reference sim')
 
     plot(fault(1).data_measured{1,1}.Time, ...
-        fault(1).data_measured{1,1}.Data, 'r', 'DisplayName', 'fault1 measured')
+        fault(1).data_measured{1,1}.Data, 'r', 'LineWidth', 2, 'DisplayName', 'fault1 measured')
     plot(fault(1).data_simulated{1,1}.Time, ...
-        fault(1).data_simulated{1,1}.Data, 'r--','DisplayName', 'fault1 simulation')
+        fault(1).data_simulated{1,1}.Data, 'r--', 'LineWidth', 2, 'DisplayName', 'fault1 simulation')
 
     plot(fault(2).data_measured{1,1}.Time, ...
-        fault(2).data_measured{1,1}.Data, 'b', 'DisplayName', 'fault2 measured')
+        fault(2).data_measured{1,1}.Data, 'b', 'LineWidth', 2, 'DisplayName', 'fault2 measured')
     plot(fault(2).data_simulated{1,1}.Time, ...
-        fault(2).data_simulated{1,1}.Data, 'b--', 'DisplayName', 'fault2 simulation')
+        fault(2).data_simulated{1,1}.Data, 'b--', 'LineWidth', 2, 'DisplayName', 'fault2 simulation')
 
     hold off
 
--- models/functions/plot_measured_simulated_data.m
+++ models/functions/plot_measured_simulated_data.m
@@ -1,9 +1,10 @@
 function [] = plot_measured_simulated_data(measured, simulated)
-    figure
+    f = figure;
+    f.Position(3:4) = [1200 600];
     subplot(3,2,1)
     hold on
-    plot(measured{1,1}.Time, measured{1,1}.Data)
-    plot(simulated{1,1}.Time, simulated{1,1}.Data)
+    plot(measured{1,1}.Time, measured{1,1}.Data, 'LineWidth', 2)
+    plot(simulated{1,1}.Time, simulated{1,1}.Data, 'LineWidth', 2)
     hold off
     title("Position")
     xlabel("Time (s)")
@@ -12,8 +13,8 @@ function [] = plot_measured_simulated_data(measured, simulated)
     
     subplot(3,2,2)
     hold on
-    plot(measured{1,2}.Time, measured{1,2}.Data)
-    plot(simulated{1,2}.Time, simulated{1,2}.Data)
+    plot(measured{1,2}.Time, measured{1,2}.Data, 'LineWidth', 2)
+    plot(simulated{1,2}.Time, simulated{1,2}.Data, 'LineWidth', 2)
     hold off
     title("Velocity")
     xlabel("Time (s)")
@@ -22,8 +23,8 @@ function [] = plot_measured_simulated_data(measured, simulated)
     
     subplot(3,2,3)
     hold on
-    plot(measured{1,3}.Time, measured{1,3}.Data)
-    plot(simulated{1,3}.Time, simulated{1,3}.Data)
+    plot(measured{1,3}.Time, measured{1,3}.Data, 'LineWidth', 2)
+    plot(simulated{1,3}.Time, simulated{1,3}.Data, 'LineWidth', 2)
     hold off
     title("Flow Extrusion")
     xlabel("Time (s)")
@@ -32,8 +33,8 @@ function [] = plot_measured_simulated_data(measured, simulated)
     
     subplot(3,2,4)
     hold on
-    plot(measured{1,4}.Time, measured{1,4}.Data)
-    plot(simulated{1,4}.Time, simulated{1,4}.Data)
+    plot(measured{1,4}.Time, measured{1,4}.Data, 'LineWidth', 2)
+    plot(simulated{1,4}.Time, simulated{1,4}.Data, 'LineWidth', 2)
     hold off
     title("Flow Contraction")
     xlabel("Time (s)")
@@ -42,8 +43,8 @@ function [] = plot_measured_simulated_data(measured, simulated)
   
     subplot(3,2,5)
     hold on
-    plot(measured{1,5}.Time, measured{1,5}.Data)
-    plot(simulated{1,5}.Time, simulated{1,5}.Data)
+    plot(measured{1,5}.Time, measured{1,5}.Data, 'LineWidth', 2)
+    plot(simulated{1,5}.Time, simulated{1,5}.Data, 'LineWidth', 2)
     hold off
     title("Proximiti sensor bottom")
     xlabel("Time (s)")
@@ -52,8 +53,8 @@ function [] = plot_measured_simulated_data(measured, simulated)
     
     subplot(3,2,6)
     hold on
-    plot(measured{1,6}.Time, measured{1,6}.Data)
-    plot(simulated{1,6}.Time, simulated{1,6}.Data)
+    plot(measured{1,6}.Time, measured{1,6}.Data, 'LineWidth', 2)
+    plot(simulated{1,6}.Time, simulated{1,6}.Data, 'LineWidth', 2)
     hold off
     title("Proximiti sensor upper")
     xlabel("Time (s)")
